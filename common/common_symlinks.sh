5582be7d6060aa119dd01c2a6215f3e54af7509f
Suvash Thapaliya <suvash@gmail.com>
2014-12-21 18:00:54 +0100

    Use weechat

--- common/common_symlinks.sh
+++ common/common_symlinks.sh
@@ -3,3 +3,4 @@ ln -s $HOME/Developer/scaffold/common/dotfiles/gitignore $HOME/.gitignore
 
 ln -s $HOME/Developer/scaffold/common/fish $HOME/.config/fish
 ln -s $HOME/Developer/scaffold/common/emacs.d $HOME/.emacs.d
+ln -s $HOME/Developer/scaffold/common/weechat $HOME/.weechat
