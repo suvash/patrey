ebfdcac63353022e8fdf84b30bd7c54b0de2ac23
Suvash Thapaliya <suvash@gmail.com>
2015-01-06 01:49:26 +0100

    Use a dotfiles folder for bajra

--- bajra/bajra_symlinks.sh
+++ bajra/bajra_symlinks.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-if [ ! -L $HOME/tmux.conf ]; then
+if [ ! -L $HOME/.tmux.conf ]; then
   rm $HOME/tmux.conf 2> /dev/null
-  ln -s $HOME/Developer/scaffold/bajra/tmux/tmux.conf $HOME/.tmux.conf
+  ln -s $HOME/Developer/scaffold/bajra/dotfiles/tmux.conf $HOME/.tmux.conf
 fi
