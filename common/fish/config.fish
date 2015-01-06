eeb6840bd443c423f24daddef2447f21159aef93
Suvash Thapaliya <suvash@gmail.com>
2015-01-06 01:08:27 +0100

    Load Nix pkgs in da path

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -3,6 +3,16 @@ set -x LC_ALL en_US.UTF-8
 
 fish_user_abbreviations
 
+switch (uname)
+  case Darwin
+    set PATH $HOME/.nix-profile/bin $PATH
+    set -x NIX_PATH $HOME/Developer/nixpkgs:nixpkgs=$HOME/Developer/nixpkgs
+  case Linux
+    echo "On a Linux."
+  case '*'
+    echo 'Whoa ! what os is this ?'
+end
+
 if test -f ~/.computer
   . ~/.computer
 end
