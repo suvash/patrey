348100c1f7ee4022b91ef0ab8cb4fb4fb7a82b4d
Suvash Thapaliya <suvash@gmail.com>
2015-01-11 18:10:27 +0100

    some thunderbolt xrandr commands

--- common/fish/functions/fish_user_abbreviations.fish
+++ common/fish/functions/fish_user_abbreviations.fish
@@ -76,5 +76,7 @@ function fish_user_abbreviations
     ducks='du -cks * | sort -rn | head' \
     cl='tty-clock' \
     sl='slimlock' \
+    tboff='xrandr --output eDP1 --mode 1440x900 --pos 0x0 --rotate normal --output DP1 --off' \
+    tbnormal='xrandr --output eDP1 --mode 1440x900 --pos 0x0 --rotate normal --output DP1 --mode 2560x1440 --pos 1440x0 --rotate normal' \
     sp='send_to_phone'
 end
