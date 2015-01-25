4063d6208db8e46cb8c5ebe9e003ba7158dc27be
Suvash Thapaliya <suvash@gmail.com>
2015-01-25 13:27:37 +0100

    Remove the protected /etc/nixos properly

--- yantra/yantra_symlinks.sh
+++ yantra/yantra_symlinks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 if [ ! -L /etc/nixos ]; then
-  rm -rf /etc/nixos 2> /dev/null
+  sudo rm -rf /etc/nixos 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/nixos/ /etc/nixos
 fi
 
