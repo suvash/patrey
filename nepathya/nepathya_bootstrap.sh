bf871da67811217d52b2f7bd32f49d24f495d0e3
Suvash Thapaliya <suvash@gmail.com>
2015-05-22 20:20:23 +0200

    Add new osx machine: nepathya

---
+++ nepathya/nepathya_bootstrap.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+# OSX-only stuff. Abort if not OSX.
+[[ "$OSTYPE" =~ ^darwin ]] || return 1
+
+# Run common_symlinks
+
+pushd $PWD
+
+# ============= Install brew ====================
+
+if [[ "$OSTYPE" =~ ^darwin ]] && [[ ! "$(type -P brew)" ]]; then
+    echo "Installing Homebrew"
+
+    #Skip the "Press enter to continue…" prompt.
+    true | ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
+fi
+
+echo "Brew bundle"
+brew doctor
+brew tap homebrew/boneyard
+
+echo "Brew bundling dependencies"
+brew bundle $HOME/Developer/scaffold/nepathya/osx/Brewfile
+
+# Add fish to shell list if not already
+if ! $(cat /etc/shells | grep -q '/usr/local/bin/fish'); then
+  echo 'Adding fish to available shells, will need privileges.'
+
+  echo '/usr/local/bin/fish' | sudo tee -a /etc/shells > /dev/null
+fi
+
+# Make fish default shell
+if ! $(finger $USER | grep -q '/usr/local/bin/fish'); then
+  echo 'Setting fish as default shell, will need privileges.'
+
+  chsh -s /usr/local/bin/fish
+fi
+
+# ============= End brew ========================
+
+popd
