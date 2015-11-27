312ff07cffc5b4f439cc6c0bc792b65d95247705
Suvash Thapaliya <suvash@gmail.com>
2015-11-27 15:33:48 +0100

    ask for sudo

--- rangamanch/rangamanch_build.sh
+++ rangamanch/rangamanch_build.sh
@@ -3,6 +3,12 @@
 # OSX-only stuff. Abort if not OSX.
 [[ "$OSTYPE" =~ ^darwin ]] || return 1
 
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `.osx` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 pushd $PWD
 
 echo "Creating $HOME/Applications"
