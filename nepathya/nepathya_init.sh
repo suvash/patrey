0ce492fd0ee0a3fadfa35ead24ee4f02ef0ab6e1
Suvash Thapaliya <suvash@gmail.com>
2015-11-27 23:28:03 +0100

    Updated settings for Nepathya

---
+++ nepathya/nepathya_init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Set computer name (as done via System Preferences ‚Üí Sharing)
+sudo scutil --set ComputerName "nepathya"
+sudo scutil --set HostName "nepathya"
+sudo scutil --set LocalHostName "nepathya"
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server NetBIOSName -string "nepathya"
+
+source $HOME/Developer/scaffold/common/common_symlinks.sh
+
+source $HOME/Developer/scaffold/osx/osx_symlinks.sh
