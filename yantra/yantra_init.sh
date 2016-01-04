2a614760e3e671e48dbaa2e9e003b9cca1e877ad
Suvash Thapaliya <suvash@gmail.com>
2016-01-04 02:19:28 +0100

    Revert "Tilde must work"
    This reverts commit 2e5dc6c74eeb5f034d6e8f30291974a69708eae2.

--- yantra/yantra_init.sh
+++ yantra/yantra_init.sh
@@ -9,6 +9,17 @@ hostnamectl set-hostname yantra
 sudo apt-get update
 sudo apt-get install -y git
 
+# Make sure to get tilde on apple keyboards
+MODPROBE_FILE=/etc/modprobe.d/hid_apple.conf
+ISO_LAYOUT_OPTION='options hid_apple iso_layout=0'
+if [ ! -f $MODPROBE_FILE ]; then
+  echo $ISO_LAYOUT_OPTION | sudo tee $MODPROBE_FILE
+else
+  if ! grep -Fxq "$ISO_LAYOUT_OPTION" $MODPROBE_FILE; then
+    echo $ISO_LAYOUT_OPTION | sudo tee -a $MODPROBE_FILE
+  fi
+fi
+
 # ============= Clone scaffold ==================
 
 if [ ! -d $HOME/Developer/scaffold/ ]; then
--- yantra/yantra_symlinks.sh
+++ yantra/yantra_symlinks.sh
@@ -1,61 +1,56 @@
 #!/usr/bin/env bash
 
 if [ ! -L $HOME/.nixpkgs ]; then
-  rm -rf $HOME/.nixpkgs
+  rm -rf $HOME/.nixpkgs 2> /dev/null
   ln -s $HOME/Developer/scaffold/yantra/nixpkgs $HOME/.nixpkgs
 fi
 
 if [ ! -L $HOME/.autorandr ]; then
-  rm -rf $HOME/.autorandr
+  rm -rf $HOME/.autorandr 2> /dev/null
   ln -s $HOME/Developer/scaffold/yantra/autorandr $HOME/.autorandr
 fi
 
 if [ ! -L $HOME/.packages.apt ]; then
-  rm -rf $HOME/.packages.apt
+  rm -rf $HOME/.packages.apt 2> /dev/null
   ln -s $HOME/Developer/scaffold/yantra/packages.apt $HOME/.packages.apt
 fi
 
-if [ ! -L /etc/rc.local ]; then
-  sudo mv /etc/rc.local /etc/rc.local.orig
-  sudo ln -s $HOME/Developer/scaffold/yantra/rc.local /etc/rc.local
-fi
-
 if [ ! -L /etc/lightdm/lightdm.conf ]; then
-  rm -rf /etc/lightdm/lightdm.conf
+  rm -rf /etc/lightdm/lightdm.conf 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/lightdm/lightdm.conf /etc/lightdm/lightdm.conf
 fi
 
 if [ ! -L /usr/share/xsessions/xmonad.desktop ]; then
-  mv /usr/share/xsessions/xmonad.desktop /usr/share/xsessions/xmonad.desktop.orig
+  mv /usr/share/xsessions/xmonad.desktop /usr/share/xsessions/xmonad.desktop.orig 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/assets/xmonad.desktop /usr/share/xsessions/xmonad.desktop
 fi
 
 if [ ! -L /usr/share/unity-greeter/xmonad_badge.png ]; then
-  rm -rf /usr/share/unity-greeter/xmonad_badge.png
+  rm -rf /usr/share/unity-greeter/xmonad_badge.png 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/assets/xmonad_badge.png /usr/share/unity-greeter/xmonad_badge.png
 fi
 
 if [ ! -L /usr/local/bin/start-xmonad-session ]; then
-  rm -rf /usr/local/bin/start-xmonad-session
+  rm -rf /usr/local/bin/start-xmonad-session 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/scripts/start-xmonad-session /usr/local/bin/start-xmonad-session
 fi
 
 if [ ! -L /usr/local/bin/start-tor-browser ]; then
-  rm -rf /usr/local/bin/start-tor-browser
+  rm -rf /usr/local/bin/start-tor-browser 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/scripts/start-tor-browser /usr/local/bin/start-tor-browser
 fi
 
 if [ ! -L /usr/local/bin/start-rstudio ]; then
-  rm -rf /usr/local/bin/start-rstudio
+  rm -rf /usr/local/bin/start-rstudio 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/scripts/start-rstudio /usr/local/bin/start-rstudio
 fi
 
 if [ ! -L /usr/local/bin/start-viber ]; then
-  rm -rf /usr/local/bin/start-viber
+  rm -rf /usr/local/bin/start-viber 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/scripts/start-viber /usr/local/bin/start-viber
 fi
 
 if [ ! -L /usr/local/bin/set-greeter-resolution ]; then
-  rm -rf /usr/local/bin/set-greeter-resolution
+  rm -rf /usr/local/bin/set-greeter-resolution 2> /dev/null
   sudo ln -s $HOME/Developer/scaffold/yantra/scripts/set-greeter-resolution /usr/local/bin/set-greeter-resolution
 fi
