7ac126816c4f50a99b35ddb0bf76bb3deac711f5
Suvash Thapaliya <suvash@gmail.com>
2016-01-04 13:06:15 +0100

    Spotify \o/

--- yantra/yantra_init.sh
+++ yantra/yantra_init.sh
@@ -58,6 +58,9 @@ sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E084DAB9
 echo "deb http://ftp.acc.umu.se/mirror/CRAN/bin/linux/ubuntu $DISTRIB_CODENAME/" \
      | sudo tee /etc/apt/sources.list.d/r.list
 
+# Spotify Client
+sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys BBEBDCB318AD50EC6865090613B00F1FD2C19886
+echo "deb http://repository.spotify.com testing non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
 
 sudo apt-get update
 
