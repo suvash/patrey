fbab224eb5997aa8c0d961691ec48d52bf948e5e
Suvash Thapaliya <suvash@gmail.com>
2016-01-12 09:33:17 +0100

    More HiDPI things

--- common/emacs.d/bootstrap/linux-setup.el
+++ common/emacs.d/bootstrap/linux-setup.el
@@ -8,6 +8,9 @@
 (setq x-select-enable-primary t)
 (setq x-select-enable-clipboard t)
 
+;;; Set font face
+(set-face-attribute 'default nil :height 100)
+
 ;;; Remap mac keys
 ;;(setq mac-function-modifier 'hyper)
 ;;(setq mac-control-modifier 'control)
