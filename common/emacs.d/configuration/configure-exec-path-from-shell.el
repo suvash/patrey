32e9aba182746b3bd3d8823bf09950dc16fa66f6
Suvash Thapaliya <suvash@gmail.com>
2017-05-23 01:58:35 +0200

    Copy env vars to emacs session

--- common/emacs.d/configuration/configure-exec-path-from-shell.el
+++ common/emacs.d/configuration/configure-exec-path-from-shell.el
@@ -1,6 +1,6 @@
 ;; fix emacs env variables to be the same as in user shell
 ;; only on osx
 
-(exec-path-from-shell-initialize)
+(exec-path-from-shell-copy-env "SSH_AUTH_SOCK")
 
 (provide 'configure-exec-path-from-shell)
