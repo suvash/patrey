2acaf7f47e58add34814e84bcc4f0e4336a7ee66
Suvash Thapaliya <suvash@gmail.com>
2017-05-23 01:19:26 +0200

    GPG2 ready

--- common/fish/config.fish
+++ common/fish/config.fish
@@ -4,6 +4,10 @@ set -x EDITOR vim
 set -x TERM xterm-256color
 set -x GPG_TTY (tty)
 
+gpg-connect-agent /bye
+gpg-connect-agent updatestartuptty /bye > /dev/null
+set -x SSH_AUTH_SOCK (gpgconf --list-dirs agent-ssh-socket)
+
 fish_user_abbreviations
 
 if test -f ~/.computer
