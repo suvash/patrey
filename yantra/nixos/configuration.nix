bcdbe4438bd2d5802629afac24363c26ba570a1c
Suvash Thapaliya <suvash@gmail.com>
2019-01-07 10:40:57 +0100

    Fix broken glibc things

--- yantra/nixos/configuration.nix
+++ yantra/nixos/configuration.nix
@@ -5,6 +5,7 @@
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
       ./gpg-yubikey-configuration.nix
+      ./multi-glibc-locale-paths.nix
     ];
 
   # Boot =================================================
