8698e03b2c3ff39e6f925279ef86308d9ea21cba
Suvash Thapaliya <suvash@gmail.com>
2019-01-30 15:21:24 +0100

    fixup! Add new packages

--- yantra/yantra_build.sh
+++ yantra/yantra_build.sh
@@ -7,16 +7,6 @@
 # Use the new configuration
 sudo nixos-rebuild switch
 
-# # Downloads scripts/packages and add to local bin
-# KUBECTX_VERSION="0.6.2"
-# if [ ! -e $HOME/.local/bin/kubectx ]; then
-#   wget --quiet -O "/tmp/kubectx.tar.gz" "https://github.com/ahmetb/kubectx/archive/v$KUBECTX_VERSION.tar.gz" \
-#   && cd "/tmp" && tar xzf "/tmp/kubectx.tar.gz" \
-#   && cp /tmp/kubectx-$KUBECTX_VERSION/kube* $HOME/.local/bin/ \
-#   && cp /tmp/kubectx-$KUBECTX_VERSION/completion/*.fish $HOME/.config/fish/completions/ \
-#   && cd - && rm -rf /tmp/kubectx-$KUBECTX_VERSION/
-# fi
-
 # Use local packages
 LOCAL_REPO="$HOME/Developer/nixpkgs"
 LOCAL_REPO_PACKAGES=()
