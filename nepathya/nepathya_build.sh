4494c9194f3927fe085a5a72153eb989b3e6dccd
Suvash Thapaliya <suvash@gmail.com>
2019-04-07 23:04:16 +0200

    Also add base16 to nepathya

--- nepathya/nepathya_build.sh
+++ nepathya/nepathya_build.sh
@@ -36,3 +36,8 @@ if ! $(finger $USER | grep -q '/usr/local/bin/fish'); then
 
   chsh -s /usr/local/bin/fish
 fi
+
+# Base16 shell download to config directory
+if [ ! -d "$HOME/.config/base16-shell" ]; then
+  git clone https://github.com/chriskempson/base16-shell.git ~/.config/base16-shell
+fi
