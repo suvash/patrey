fe55f7a3dfac79d9017be963c645d3ea60f641f9
Suvash Thapaliya <suvash@gmail.com>
2020-02-06 01:06:06 +0100

    Add ubuntu-drivers in init

--- prayogshala/prayogshala_init.sh
+++ prayogshala/prayogshala_init.sh
@@ -5,10 +5,13 @@ pushd $PWD
 # Set computer properties
 hostnamectl set-hostname prayogshala
 
-
 # Add ppa graphics drivers repository
 sudo add-apt-repository ppa:graphics-drivers/ppa --yes
 
+# Ubuntu drivers
+sudo apt install ubuntu-drivers-common --yes
+echo "Check the recommendation below..."
+ubuntu-drivers devices
 
 # Symlinks
 source $HOME/Developer/scaffold/prayogshala/prayogshala_symlinks.sh
