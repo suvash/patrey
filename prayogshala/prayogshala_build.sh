2371ec89d08d85f332557621b8acfec1bcedc927
Suvash Thapaliya <suvash@gmail.com>
2020-02-06 01:12:29 +0100

    Fix typo

--- prayogshala/prayogshala_build.sh
+++ prayogshala/prayogshala_build.sh
@@ -10,7 +10,7 @@ cat "$HOME/.packages.apt" | awk '!/(^#|^$)/{print $0}' | xargs sudo apt install 
 sudo apt autoremove -y
 
 # Install miniconda
-if ! hash autorandr; then
+if ! hash conda; then
     wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O /tmp/miniconda.sh
     sudo bash /tmp/miniconda.sh -b -p /opt/miniconda
     sudo ln -sfv /opt/miniconda/etc/profile.d/conda.sh /etc/profile.d/conda.sh
