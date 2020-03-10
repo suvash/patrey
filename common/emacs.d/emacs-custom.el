2aa05c7afa7ab056624ef26cc09915e259a40ee1
Suvash Thapaliya <suvash@gmail.com>
2020-03-10 02:29:55 +0100

    Update emacs custom

--- common/emacs.d/emacs-custom.el
+++ common/emacs.d/emacs-custom.el
@@ -7,7 +7,7 @@
  '(blacken-skip-string-normalization t)
  '(package-selected-packages
    (quote
-    (elpy base16-theme cider clojure-mode sqlup-mode sql-indent yaml-mode json-mode web-mode markdown-mode nyan-mode go-mode enh-ruby-mode blacken anaconda-mode nix-mode terraform-mode restclient fish-mode dumb-jump company flycheck-yamllint flycheck ag git-timemachine git-gutter-fringe+ git-gutter+ magit evil-smartparens evil-leader evil smartparens linum-relative rainbow-delimiters smooth-scrolling yasnippet guide-key smart-mode-line smex ido-vertical-mode ido-completing-read+ ido-hacks flx-ido persp-projectile perspective projectile exec-path-from-shell))))
+    (typescript-mode py-isort elpy base16-theme cider clojure-mode sqlup-mode sql-indent yaml-mode json-mode web-mode markdown-mode nyan-mode go-mode enh-ruby-mode blacken anaconda-mode nix-mode terraform-mode restclient fish-mode dumb-jump company flycheck-yamllint flycheck ag git-timemachine git-gutter-fringe+ git-gutter+ magit evil-smartparens evil-leader evil smartparens linum-relative rainbow-delimiters smooth-scrolling yasnippet guide-key smart-mode-line smex ido-vertical-mode ido-completing-read+ ido-hacks flx-ido persp-projectile perspective projectile exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
