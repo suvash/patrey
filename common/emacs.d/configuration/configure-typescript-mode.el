fa43043019d6f1ed1db65c2a18015729707d229c
Suvash Thapaliya <suvash@gmail.com>
2020-03-10 16:36:01 +0100

    Add tsx files to ts-mode

--- common/emacs.d/configuration/configure-typescript-mode.el
+++ common/emacs.d/configuration/configure-typescript-mode.el
@@ -2,6 +2,8 @@
 
 (require 'typescript-mode)
 
+(add-to-list 'auto-mode-alist '("\\.tsx?\\'" . typescript-mode))
+
 (add-hook 'typescript-mode-hook
       (lambda ()
         (setq typescript-indent-level 2)))
