05bfced51901d38553cc72ffad3bdb6aceb4d907
Suvash Thapaliya <suvash@gmail.com>
2020-03-12 01:22:51 +0100

    Use safe bash syntax

--- prayogshala/prayogshala_symlinks.sh
+++ prayogshala/prayogshala_symlinks.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
-mkdir -p $HOME/.config
-mkdir -p $HOME/.config/fish
+mkdir -p "$HOME/.config"
+mkdir -p "$HOME/.config/fish"
 
 if [ ! -L "$HOME/.packages.apt" ]; then
   rm -rf "$HOME/.packages.apt" 2> /dev/null
@@ -18,11 +18,11 @@ if [ ! -L "$HOME/.gitignore" ]; then
   ln -sfnv "$HOME/Developer/scaffold/prayogshala/dotfiles/gitignore" "$HOME/.gitignore"
 fi
 
-if [ ! -L $HOME/.config/fish/config.fish ]; then
-  rm -rf $HOME/.config/fish/config.fish 2> /dev/null
-  rm -rf $HOME/.config/fish/fishfile 2> /dev/null
-  ln -sfnv $HOME/Developer/scaffold/common/fish/config.fish $HOME/.config/fish/config.fish
-  ln -sfnv $HOME/Developer/scaffold/common/fish/fishfile $HOME/.config/fish/fishfile
+if [ ! -L "$HOME/.config/fish/config.fish" ]; then
+  rm -rf "$HOME/.config/fish/config.fish" 2> /dev/null
+  rm -rf "$HOME/.config/fish/fishfile" 2> /dev/null
+  ln -sfnv "$HOME/Developer/scaffold/common/fish/config.fish" "$HOME/.config/fish/config.fish"
+  ln -sfnv "$HOME/Developer/scaffold/common/fish/fishfile" "$HOME/.config/fish/fishfile"
   mkdir -p $HOME/.config/fish/{completions,functions}
   for file in $HOME/Developer/scaffold/common/fish/completions/*
   do
@@ -34,12 +34,12 @@ if [ ! -L $HOME/.config/fish/config.fish ]; then
   done
 fi
 
-if [ ! -L $HOME/.emacs.d ]; then
-  rm -rf $HOME/.emacs.d/ 2> /dev/null
-  ln -sfnv $HOME/Developer/scaffold/common/emacs.d $HOME/.emacs.d
+if [ ! -L "$HOME/.emacs.d" ]; then
+  rm -rf "$HOME/.emacs.d/" 2> /dev/null
+  ln -sfnv "$HOME/Developer/scaffold/common/emacs.d" "$HOME/.emacs.d"
 fi
 
-if [ ! -L $HOME/.vim ]; then
-  rm -rf $HOME/.vim 2> /dev/null
-  ln -sfnv $HOME/Developer/scaffold/common/vim $HOME/.vim
+if [ ! -L "$HOME/.vim" ]; then
+  rm -rf "$HOME/.vim" 2> /dev/null
+  ln -sfnv "$HOME/Developer/scaffold/common/vim" "$HOME/.vim"
 fi
