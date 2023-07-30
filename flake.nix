bbe45b85360e88a3501cf8173545ba02c9fe30c2
Suvash Thapaliya <suvash@gmail.com>
2023-07-31 00:18:54 +0200

    basic nixos + flakes + home-manager setup

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "Suvash's NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/release-23.05";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }@inputs:
+    let
+      system = "x86_64-linux";
+    in
+    {
+      # sudo nixos-rebuild switch --flake .#hostname
+      nixosConfigurations = {
+        paathshala = nixpkgs.lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./nixos/hosts/paathshala/configuration.nix
+          ];
+        };
+      };
+
+      # First time : nix run home-manager/release-23.05 -- switch --flake .#username@hostname
+      # Then after : home-manager switch --flake .#username@hostname
+      homeConfigurations = {
+        "suvash@paathshala" = home-manager.lib.homeManagerConfiguration {
+          pkgs = nixpkgs.legacyPackages.${system}; # required by home-manager
+          modules = [ ./home.nix ];
+        };
+      };
+
+    };
+}
---
+++ home.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  programs.home-manager.enable = true;
+
+  home.stateVersion = "23.05";
+
+  home.username = "suvash";
+  home.homeDirectory = "/home/suvash";
+
+  home.packages = with pkgs; [
+    cmatrix
+    nixpkgs-fmt
+  ];
+}
