80f75429970c3d94c7fb6483d6b6d9ba8c87c904
Suvash Thapaliya <suvash@gmail.com>
2023-10-24 19:13:50 +0200

    add nix cache

--- flake.nix
+++ flake.nix
@@ -1,6 +1,18 @@
 {
   description = "Suvash's NixOS Flake";
 
+  nixConfig = {
+    substituters = [
+      "https://cache.nixos.org/"
+    ];
+    extra-substituters = [
+      "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
+  };
+
   inputs = {
     nixpkgs_unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs_2305.url = "github:nixos/nixpkgs/nixos-23.05";
