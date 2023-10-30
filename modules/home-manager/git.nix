68e70bbe28e853ed90c5448e3605ea7bd31412d8
Suvash Thapaliya <suvash@gmail.com>
2023-10-30 09:13:02 +0545

    Git config

--- modules/home-manager/git.nix
+++ modules/home-manager/git.nix
@@ -1,6 +1,16 @@
 {pkgs, ...}: {
   programs.git = {
     enable = true;
+    userEmail = "suvash@gmail.com";
+    userName = "Suvash Thapaliya";
+    signing = {
+      key = "5E73D2B2";
+      signByDefault = false;
+    };
+    ignores = [
+      "DS_Store"
+      "*.swp"
+    ];
     lfs.enable = true;
     diff-so-fancy.enable = true;
   };
