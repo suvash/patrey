474df0fe9df136c71943c10c5172ab5e043a9168
Suvash Thapaliya <suvash@gmail.com>
2023-11-12 13:18:40 +0545

    keybind lsp format buffer

--- modules/home-manager/neovim/lsp-zero-nvim.lua
+++ modules/home-manager/neovim/lsp-zero-nvim.lua
@@ -1,8 +1,12 @@
 local lsp_zero = require("lsp-zero")
 
 lsp_zero.on_attach(function(client, bufnr)
-  lsp_zero.default_keymaps({ buffer = bufnr })
-  print(client.name .. ": LSP attached !")
+  local opts = { buffer = bufnr }
+  lsp_zero.default_keymaps(opts)
+
+  vim.keymap.set({ 'n', 'x' }, '<leader>fb', function()
+    vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+  end, opts)
 end)
 
 lsp_zero.set_sign_icons({
