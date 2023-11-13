4b4bd5ac51a18a76eb5f5eb6540497f4c8fd8b71
Suvash Thapaliya <suvash@gmail.com>
2023-11-13 13:33:28 +0545

    Some cleanup and enable all locales (in case)

--- hosts/paathshala/configuration.nix
+++ hosts/paathshala/configuration.nix
@@ -1,6 +1,3 @@
-# Edit this configuration file to define what should be installed on
-# your system.  Help is available in the configuration.nix(5) man page
-# and in the NixOS manual (accessible by running `nixos-help`).
 {
   inputs,
   outputs,
@@ -132,7 +129,10 @@
   services.fwupd.enable = true;
 
   # Select internationalisation properties.
-  i18n = {defaultLocale = "en_US.UTF-8";};
+  i18n = {
+    defaultLocale = "en_GB.UTF-8";
+    supportedLocales = ["all"];
+  };
 
   console = {
     font = "latarcyrheb-sun32";
