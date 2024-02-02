456c6339265fda2864d87d4d5d5ce3392e42bacc
Suvash Thapaliya <suvash@gmail.com>
2024-02-02 02:18:29 +0100

    max cpu 80% on battery

--- hosts/paathshala/configuration.nix
+++ hosts/paathshala/configuration.nix
@@ -348,7 +348,7 @@
       CPU_MIN_PERF_ON_AC = 10;
       CPU_MAX_PERF_ON_AC = 100;
       CPU_MIN_PERF_ON_BAT = 10;
-      CPU_MAX_PERF_ON_BAT = 60;
+      CPU_MAX_PERF_ON_BAT = 80;
       CPU_BOOST_ON_AC = 1;
       CPU_BOOST_ON_BAT = 0;
       CPU_HWP_DYN_BOOST_ON_AC = 1;
