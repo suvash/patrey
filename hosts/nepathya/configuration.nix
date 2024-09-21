872259f0d18109c366b8798b00041a6b748360cc
Suvash Thapaliya <suvash@gmail.com>
2024-09-21 14:40:44 +0200

    Fix PATH issue in fish

--- hosts/nepathya/configuration.nix
+++ hosts/nepathya/configuration.nix
@@ -31,6 +31,20 @@
   programs.zsh.enable = true;
   programs.fish.enable = true;
 
+  # Needed to address bug where $PATH is not properly set for fish:
+  # https://github.com/LnL7/nix-darwin/issues/122#issuecomment-1659465635
+  programs.fish.loginShellInit = let
+    # This naive quoting is good enough in this case. There shouldn't be any
+    # double quotes in the input string, and it needs to be double quoted in case
+    # it contains a space (which is unlikely!)
+    dquote = str: "\"" + str + "\"";
+
+    makeBinPathList = map (path: path + "/bin");
+  in ''
+    fish_add_path --move --prepend --path ${lib.concatMapStringsSep " " dquote (makeBinPathList config.environment.profiles)}
+    set fish_user_paths $fish_user_paths
+  '';
+
   # NETWORKING ======================================================================
 
   networking = {
