680d910ddf0698be6995487dcfe551162aa454f4
Suvash Thapaliya <suvash@gmail.com>
2025-05-04 00:52:22 +0200

    more macos settings

--- hosts/nepathya/macos.nix
+++ hosts/nepathya/macos.nix
@@ -31,5 +31,68 @@
       wvous-tl-corner = 4; # Desktop
       wvous-tr-corner = 12; # Notification Center
     };
+
+    ".GlobalPreferences" = {
+      "com.apple.sound.beep.sound" = "/System/Library/Sounds/Funk.aiff";
+    };
+
+    ActivityMonitor = {
+      IconType = 6; # CPU Usage
+      OpenMainWindow = true;
+      ShowCategory = 100; # All Processes
+      SortColumn = "CPUUsage";
+      SortDirection = 0; # Descending
+    };
+
+    LaunchServices.LSQuarantine = true;
+
+    NSGlobalDomain = {
+      AppleEnableSwipeNavigateWithScrolls = true;
+      AppleEnableMouseSwipeNavigateWithScrolls = true;
+      AppleInterfaceStyleSwitchesAutomatically = true;
+      AppleKeyboardUIMode = 3;
+      ApplePressAndHoldEnabled = false;
+      AppleScrollerPagingBehavior = true;
+      AppleShowAllExtensions = true;
+      AppleShowAllFiles = true;
+      AppleShowScrollBars = "Always";
+      AppleSpacesSwitchOnActivate = true;
+      AppleTemperatureUnit = "Celsius";
+      AppleMetricUnits = 1;
+      AppleMeasurementUnits = "Centimeters";
+      AppleWindowTabbingMode = "fullscreen";
+      InitialKeyRepeat = 5;
+      KeyRepeat = 1;
+      NSAutomaticDashSubstitutionEnabled = false;
+      NSAutomaticInlinePredictionEnabled = false;
+      NSAutomaticPeriodSubstitutionEnabled = false;
+      NSAutomaticQuoteSubstitutionEnabled = false;
+      NSAutomaticSpellingCorrectionEnabled = false;
+      NSAutomaticWindowAnimationsEnabled = false;
+      NSDisableAutomaticTermination = true;
+      NSDocumentSaveNewDocumentsToCloud = true;
+      NSNavPanelExpandedStateForSaveMode = true;
+      NSNavPanelExpandedStateForSaveMode2 = true;
+      NSScrollAnimationEnabled = true;
+      NSTableViewDefaultSizeMode = 2;
+      NSTextShowsControlCharacters = true;
+      NSUseAnimatedFocusRing = false;
+      NSWindowResizeTime = 0.001;
+      PMPrintingExpandedStateForPrint = true;
+      PMPrintingExpandedStateForPrint2 = true;
+      _HIHideMenuBar = false;
+      "com.apple.keyboard.fnState" = false;
+      "com.apple.mouse.tapBehavior" = 1;
+      "com.apple.sound.beep.feedback" = 0;
+      "com.apple.sound.beep.volume" = 0.7788;
+      "com.apple.springing.delay" = 0.0;
+      "com.apple.springing.enabled" = true;
+      "com.apple.swipescrolldirection" = true;
+      "com.apple.trackpad.enableSecondaryClick" = true;
+      "com.apple.trackpad.forceClick" = true;
+      "com.apple.trackpad.scaling" = 1.5;
+    };
+
+
   };
 }
